32bed5840a28d5186991d468e133b4ac5a10df66
Bhaskar Reddy <164631537+Bhas90@users.noreply.github.com>
2025-02-26 17:17:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors({
-  origin: "https://rugby.britishelderlycare.com/", // Allow frontend domain
+  origin: "*", // Allow frontend domain
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true // If using cookies or authentication headers
